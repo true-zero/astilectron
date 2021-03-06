caa39bac614bd182d7692869df0519201d37d1df
true-zero <iamtruezero@gmail.com>
2021-03-06 11:51:42 +0000

    Added set proxy, load url, intercept string protocol

--- src/consts.js
+++ src/consts.js
@@ -4,6 +4,7 @@ module.exports = {
     callbackNames: {
         webContentsLogin: "web.contents.login",
         webContentsMessage: "web.contents.message",
+        webContentsInterceptStringProtocol: "web.contents.intercept.string.protocol",
     },
     eventNames: {
         appCmdQuit: "app.cmd.quit",
@@ -76,6 +77,8 @@ module.exports = {
         trayEventRightClicked: "tray.event.right.clicked",
         webContentsEventLogin: "web.contents.event.login",
         webContentsEventLoginCallback: "web.contents.event.login.callback",
+        webContentsEventInterceptStringProtocol: "web.contents.event.intercept.string.protocol",
+        webContentsEventInterceptStringProtocolCallback: "web.contents.event.intercept.string.protocol.callback",
         windowCmdBlur: "window.cmd.blur",
         windowCmdCenter: "window.cmd.center",
         windowCmdClose: "window.cmd.close",
@@ -93,11 +96,14 @@ module.exports = {
         windowCmdSetBounds: "window.cmd.set.bounds",
         windowCmdRestore: "window.cmd.restore",
         windowCmdShow: "window.cmd.show",
+        windowCmdLoadUrl: "window.cmd.load.url",
         windowCmdUnmaximize: "window.cmd.unmaximize",
         windowCmdUpdateCustomOptions: "window.cmd.update.custom.options",
         windowCmdWebContentsCloseDevTools: "window.cmd.web.contents.close.dev.tools",
         windowCmdWebContentsOpenDevTools: "window.cmd.web.contents.open.dev.tools",
         windowCmdWebContentsExecuteJavascript: "window.cmd.web.contents.execute.javascript",
+        windowCmdWebContentsInterceptStringProtocol: "window.cmd.web.contents.intercept.string.protocol",
+        windowCmdWebContentsSetProxy: "window.cmd.web.contents.set.proxy",
         windowEventBlur: "window.event.blur",
         windowEventClosed: "window.event.closed",
         windowEventDidFinishLoad: "window.event.did.finish.load",
@@ -116,7 +122,10 @@ module.exports = {
         windowEventShow: "window.event.show",
         windowEventUnmaximize: "window.event.unmaximize",
         windowEventUnresponsive: "window.event.unresponsive",
+        windowEventLoadedUrl: "window.event.loaded.url",
         windowEventWebContentsExecutedJavaScript: "window.event.web.contents.executed.javascript",
+        windowEventWebContentsSetProxy: "window.event.web.contents.set.proxy",
+        windowEventWebContentsInterceptStringProtocol: "window.event.web.contents.intercept.string.protocol",
         windowEventUpdatedCustomOptions: "window.event.updated.custom.options"
     },
     targetIds: {
